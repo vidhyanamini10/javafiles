085cc9c2c2826b84207ba7d42e7dd7aa3dc6fdcb
vidhyanamini10 <124749085+vidhyanamini10@users.noreply.github.com>
2024-01-31 15:34:44 -0600

    added new files

--- src/main/java/functionalinterface/Character.java
+++ src/main/java/functionalinterface/Character.java
@@ -1,2 +1,24 @@
-package functionalinterface;public class Character {
+package functionalinterface;
+
+public class Character {
+
+
+        public static void identifyAndPrintRepeated(String input) {
+            if (input == null) {
+                throw new IllegalArgumentException("Input string cannot be null.");
+            }
+
+            char[] chars = input.toCharArray();
+
+            System.out.println("Repeated characters in the string:");
+
+            for (int i = 0; i < chars.length; i++) {
+                for (int j = i + 1; j < chars.length; j++) {
+                    if (chars[i] == chars[j]) {
+                        System.out.print(chars[i] + " ");
+                        break;
+                    }
+                }
+            }
+        }
 }
--- src/main/java/functionalinterface/IntegerValue.java
+++ src/main/java/functionalinterface/IntegerValue.java
@@ -1,2 +1,16 @@
-package functionalinterface;public class IntegerValue {
-}
+package functionalinterface;
+
+public class IntegerValue {
+
+
+        public static void exchangeValues(int x, int y) {
+            System.out.println("Before exchanging: x = " + x + ", y = " + y);
+
+            x = x + y;
+            y = x - y;
+            x = x - y;
+
+            System.out.println("After exchanging: x = " + x + ", y = " + y);
+        }
+    }
+
