a5bf1842c170bd8d870f314721a1ab27b243a471
vidhyanamini10 <124749085+vidhyanamini10@users.noreply.github.com>
2024-02-01 16:04:33 -0600

    added new files

--- src/test/java/functionalinterface/ListReversalTest.java
+++ src/test/java/functionalinterface/ListReversalTest.java
@@ -1,2 +1,25 @@
-package functionalinterface;public class ListReversalTest {
+package functionalinterface;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListReverserTest {
+
+    @Test
+    public void testReverseList() {
+        List<String> original = new ArrayList<>();
+        original.add("Hello");
+        original.add("World");
+        original.add("!");
+
+        List<String> reversed = ListReversal.reverseList(original);
+
+        assertEquals(3, reversed.size());
+        assertEquals("!", reversed.get(0));
+        assertEquals("World", reversed.get(1));
+        assertEquals("Hello", reversed.get(2));
+    }
 }
