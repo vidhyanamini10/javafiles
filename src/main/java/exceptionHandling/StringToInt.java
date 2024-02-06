666712ae2a86b742b5f1bf38ae640ad679cfc257
vidhyanamini10 <124749085+vidhyanamini10@users.noreply.github.com>
2024-02-06 15:31:17 -0600

    added new files

--- src/main/java/exceptionHandling/StringToInt.java
+++ src/main/java/exceptionHandling/StringToInt.java
@@ -1,2 +1,18 @@
-package exceptionHandling;public class StringToInt {
+package exceptionHandling;
+
+public class StringToInt   {
+
+    public static String convertStringToInt(String str) throws NumberFormatException {
+        try {
+            return ""+Integer.parseInt(str);
+        }
+        catch(NumberFormatException ex){
+            return "Inside the Exception \t"+ex.getMessage();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println( StringToInt.convertStringToInt("abc"));
+    }
 }
+
